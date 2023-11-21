96c2eebbad2b0d8af2fdc61bbed9a64a476c6ce2
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-11-21 19:49:36 +0530

    new link

--- src/Base/Navbar.js
+++ src/Base/Navbar.js
@@ -108,7 +108,7 @@ const Navbar = () => {
             <li className='nav-item'>
               <a 
               className='nav-link resume-a'
-              href="https://drive.google.com/file/d/12ApEBHqAtB8xUwTzTNvlL-9dlto-9bRp/view?usp=sharing" 
+              href="https://drive.google.com/file/d/1HaTLxEM3vgglOTlhJQOSxFP03mtDlQkg/view?usp=sharing" 
               target="_blank"
               >Resume</a>
             </li>
